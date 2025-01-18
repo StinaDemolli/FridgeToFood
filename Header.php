54b6aa56ecdd450b1f53b891aa24f060f1ae993c
StinaDemolli <sd70312@ubt-uni.net>
2025-01-18 23:50:15 +0100

    changes

---
+++ Header.php
@@ -0,0 +1,30 @@
+<div class="Header">
+        <header>
+
+            <div class="Logo" >
+        <img src="Images/logoFf.png" alt="logo">
+            </div>
+
+    <nav class="Navigimi">
+        <div class="Menus">
+
+            <ul>
+                <li class="crumb"><a href="HomePage.html"> Home </a></li>
+                <li class="crumb"><a href="recipes.html"> Recipes </a></li>
+                <li class="crumb"><a href="AboutUs.html"> About Us </a></li>
+                <li class="crumb"><a href="contact.html"> Contact </a></li>
+
+            </ul>
+            
+        </div>
+    </nav>
+
+        <div class="BigBut">
+            <a href="Login.html" class="LogButt">Login</a>
+            </div>
+    
+
+
+        </header>
+
+    </div>

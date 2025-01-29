0494a8f7787fc122c4b9c37e93c116338ea25cc3
VeseVrellaku <vv71793@ubt-uni.net>
2025-01-29 21:13:55 +0100

    krijimi i databazes me OOP

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "web_projekti"; 
+class Database {
+    private $host = "localhost";
+    private $dbname = "web_projekti";
+    private $username = "root";
+    private $password = "";
+    private $conn;
 
-$conn = new mysqli($servername, $username, $password, $database);
+    public function __construct() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", 
+                                  $this->username, 
+                                  $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    public function getConnection() {
+        return $this->conn;
+    }
 }
 ?>

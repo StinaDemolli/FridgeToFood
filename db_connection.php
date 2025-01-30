2d279061c4937a04f1b28cd6e52f6f8a6170c81f
StinaDemolli <sd70312@ubt-uni.net>
2025-01-30 16:58:26 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 $servername = "localhost"; 
 $db = "web_projekti";
 $username = "root"; 
@@ -12,5 +13,16 @@ try {
     echo "Connected successfully!";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
+=======
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "web_projekti"; 
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+>>>>>>> parent of 0494a8f (krijimi i databazes me OOP)
 }
 ?>
